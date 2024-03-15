a02a653e27017bfd90633e7f4dbae0e99d11d7a1
sabrinaz35 <sj.zuurbier@gmail.com>
2024-03-15 12:41:21 +0100

    aanpassingen

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let joystatus = document.querySelector(".joystatus");
 let sleepingstatus = document.querySelector(".sleepingstatus");
 let happystatus = document.querySelector(".happystatus");
 
-let honger = 100 //waarde van de progressbalk start op nul 
+let honger = 100 //waarde van de progressbalk
 let joy = 100
 let sleep = 100
 let happy = 100
@@ -35,12 +35,9 @@ let audioSnurk = new Audio ("sounds/pug-roncando-95042.mp3")
 let audioBall = new Audio ("sounds/bouncing-ball.mp3")
 
 function geefEten() { // maak een functie aan voor de hongerbalk
-  if (honger == 100) {
-    h1element.textContent = "OEF daar heb ik nu geen behoefte aan" // dit komt in beeld als het 100 is
-  } else {
-    honger = honger + 5 // dit zorgt ervoor dat er steeds 1 bij de honger komt
-    hungerstatus.style.width = honger + "%" //hiermee koppel je de div met de class aan het groeien van de balk
-  }
+  honger = honger + 5 // dit zorgt ervoor dat er steeds 1 bij de honger komt
+  hungerstatus.style.width = honger + "%" //hiermee koppel je de div met de class aan het groeien van de balk
+
   joy = joy - 1 //omdat je hem eten geeft wordt de rest minder om het terug te zien noteer je eerst de variabele en daarna pas wat je wilt doen
   console.log(joystatus.style.width)
   joystatus.style.width = joy + "%"
@@ -55,12 +52,9 @@ function geefEten() { // maak een functie aan voor de hongerbalk
 }
 
 function geefAai() {
-  if (joy == 100) {
-    h1element.textContent = "Blijf van mij af, ik wil niet geaaid worden"
-  } else {
-    joy = joy + 5 //als ik op 5 zet gaat hij buiten de border hoe moet ik dit aanpassen? 
-    joystatus.style.width = joy + "%"
-  }
+  joy = joy + 5 //als ik op 5 zet gaat hij buiten de border hoe moet ik dit aanpassen? 
+  joystatus.style.width = joy + "%"
+
   honger = honger -1 
   hungerstatus.style.width = honger + "%"
   sleep = sleep - 1
@@ -72,12 +66,9 @@ function geefAai() {
 }
 
 function gaSlapen() {
-  if (sleep == 100) {
-    h1element.textContent = "Ik ben helemaal niet moe"
-  } else {
-    sleep = sleep + 5
-    sleepingstatus.style.width = sleep + "%"
-  }
+  sleep = sleep + 5
+  sleepingstatus.style.width = sleep + "%"
+
   honger = honger - 1 
   hungerstatus.style.width = honger + "%"
   joy = joy - 1
@@ -89,12 +80,9 @@ function gaSlapen() {
 }
 
 function gooiBal() {
-  if (happy == 100) {
-    h1element.textContent = "Stop met die bal gooien ik heb geen zin meer"
-  } else {
-    happy = happy + 5
-    happystatus.style.width = happy + "%"
-  }
+  happy = happy + 5
+  happystatus.style.width = happy + "%"
+  
   honger = honger - 1 
   hungerstatus.style.width = honger + "%"
   joy = joy - 1
@@ -105,27 +93,60 @@ function gooiBal() {
   audioBall.play ()
 }
 
-//timer toevoegen aan de bar, zodat deze afloopt
-setInterval (timer1, 1000) //activeert de function timer, het getal is in milliseconden
+//timer toevoegen aan de bar, zodat deze afloopt, elke moet apart aangezet worden zodat ze apart lopen en stilgezet kunnen worden.
+let tmrEten = setInterval (timerEten, 500) //activeert de function timer, het getal is in milliseconden
+let tmrAai = setInterval (timerAai, 500)
+let tmrBal = setInterval (timerBal, 500)
+let tmrSlapen = setInterval (timerSlapen, 500)
 
-function timer1(){ //in deze functie gaat er steeds 1 vanaf gedurende de seconde
-  honger = honger -1 
+//in deze functie gaat er steeds 1 vanaf gedurende de seconde, elke timer een aparte functie om ze apart te kunnen laten stoppen.
+function timerEten(){ 
+  if (honger <= 5) {
+    stopTimer (tmrEten)
+  }
+  else {honger = honger -1 
   hungerstatus.style.width = honger + "%"
-  happy = happy -1
-  happystatus.style.width = happy + "%"
-  joy = joy -1
-  joystatus.style.width = joy+ "%"
-  sleep = sleep -1
-  sleepingstatus.style.width = sleep + "%"
+ }
   colorChangeBar()
 }
 
-function stopTimer(){ //om de timer te laten stoppen bij 1 
-  clearInterval (timer1)
+function timerAai(){ 
+  if (joy <= 5){
+    stopTimer(tmrAai) 
+  } else {
+  joy = joy -1
+  joystatus.style.width = joy+ "%"
+}
+  colorChangeBar()
+}
+
+function timerSlapen(){ 
+  if (sleep <= 5){
+    stopTimer(tmrSlapen) 
+  } else { 
+    sleep = sleep -1
+   sleepingstatus.style.width = sleep + "%" 
+  }
+  colorChangeBar()
+}
+
+function timerBal(){ 
+  if (happy <= 5 ){
+    stopTimer(tmrBal) 
+  } else {
+    happy = happy -1
+    happystatus.style.width = happy + "%"
+  }
+  colorChangeBar()
+}
+
+//om de timer te laten stoppen
+function stopTimer(timer){ 
+  clearInterval (timer)
 }
 
 // het veranderen van kleur bij de progressiebar + het veranderen van de emotie van de hond
-function colorChangeBar (){
+function colorChangeBar(){
   console.log(honger);
   if (honger <= 20 || happy <= 20 || joy <= 20 || sleep <= 20){
     hungerstatus.style.backgroundColor = 'red' //het aangeven van een andere kleur aan de balk zodra er iets anders gebeurd
@@ -134,6 +155,7 @@ function colorChangeBar (){
     sleepingstatus.style.backgroundColor = 'red' 
     document.querySelector(".hond").src= 'images/'+plaatjeHondArray[4];
     h1element.textContent = "Noem je dit verzorgen!"
+    console.log ("1 " + honger + " " + happy + " " + joy + " " + sleep)
   } else if (honger <= 45 || happy <= 45 || joy <= 45 || sleep <= 45){
     hungerstatus.style.backgroundColor = 'orange'
     happystatus.style.backgroundColor = 'orange'
@@ -141,26 +163,18 @@ function colorChangeBar (){
     sleepingstatus.style.backgroundColor = 'orange'
     document.querySelector(".hond").src= 'images/'+plaatjeHondArray[1];
     h1element.textContent = "Let je wel op! Ik wil graag dat je mij verzorgt"
-  } else if (honger || happy || joy || sleep == 5){
-    stopTimer()
-    document.querySelector(".hond").src= 'images/'+plaatjeHondArray[0];
-  } else { //Hij verandert nog niet terug naar groen... Als hij weer boven het getal komt.
+    console.log ("2 " + honger + " " + happy + " " + joy + " " + sleep)
+  }  else { //Hij verandert nog niet terug naar groen... Als hij weer boven het getal komt.
     hungerstatus.style.backgroundColor = 'green'
     happystatus.style.backgroundColor = 'green'
     joystatus.style.backgroundColor = 'green'
     sleepingstatus.style.backgroundColor = 'green'
-    h1element.textContent = "Je bent zo lief!!"
+    console.log ("4 " + honger + " " + happy + " " + joy + " " + sleep)
   } 
 }
 colorChangeBar()
-
 
 hand.addEventListener ("click", geefAai)
 hondenMand.addEventListener ("click", gaSlapen)
 hondenVoer.addEventListener ("click", geefEten)
 tennisbal.addEventListener ("click", gooiBal)
-
-
-//  let audioEten = new Audio ("sounds/eating-sound-effect-36186.mp3") //Het ophalen van de audio uit je documenten
-// audioEten.play()
-//Het aanspreken om het te laten doen, deze audio kan niet in dezelfde functie geplaatst worden.
